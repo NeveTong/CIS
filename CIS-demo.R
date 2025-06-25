dd2f7a40ca2ef3d050f147d1bb3f54eb3a4d46b2
NeveTong <31454255+NeveTong@users.noreply.github.com>
2025-06-25 13:38:06 -0400

    Update CIS-demo.R

--- CIS-demo.R
+++ CIS-demo.R
@@ -7,5 +7,7 @@ s <- 1
 X <- matrix(rnorm(n * p), nrow = n, ncol = p)
 Y <- matrix(rnorm(n), nrow = n, ncol = 1)
 Z <- matrix(rnorm(n * s), nrow = n, ncol = s)
-h <- mkde.tune(Z)$hopt
+h <- density(Z)$bw         # kernel bandwidth, for one-dimensional Z
+# h <- mkde.tune(Z)$hopt   # for two-dimensional Z
+
 CIS(X, Y, Z, h)          # CIS marginal utility measure
